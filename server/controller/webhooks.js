e6a70dd9d19e247e0c78424a4c048ff3a67bebe4
Prince Kumar <dhimanprince262@gmail.com>
2026-03-11 07:26:18 +0530

    issue in clerk to create user in DB

--- server/controller/webhooks.js
+++ server/controller/webhooks.js
@@ -11,7 +11,7 @@ export const clerkWebhooks = async (req, res) => {
             "svix-id": req.headers["svix-id"],
             "svix-signature": req.headers["svix-signature"],
             "svix-timestamp": req.headers["svix-timestamp"]
-        })
+        });
 
         const { data, type } = req.body;
 
@@ -19,7 +19,7 @@ export const clerkWebhooks = async (req, res) => {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
